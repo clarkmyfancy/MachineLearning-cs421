3982a7df984571ebab32f99dd1815f203643e8fb
clarkmyfancy <clark.jack0@gmail.com>
2019-10-19 17:59:05 -0500

    added features and test data

--- PredictingForestFires/script.py
+++ PredictingForestFires/script.py
@@ -1,6 +1,22 @@
 
 def main():
-    pass
+    test_data = 'hw3_question1.csv'
+
+    features = [
+        'x-axis spatial coordinate of forest',    # [1,9]
+        'y-axis spatial coordinate of forest',    # [2,9]
+        'Month',                                  # [1,12]
+        'Day',                                    # [1,7]    
+        'FFMC',                                   # (index from FWI system)
+        'DMC',                                    # (...)
+        'DC',                                     # (...)
+        'ISI',                                    # (...)
+        'Temperature',                            # in Celcius
+        'Relative Humidity',
+        'Wind Speed',                             # km/h   
+        'Rain',                                   # mm/m^2
+        'Area Burned'
+    ]
 
 
 
