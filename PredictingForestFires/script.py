85cb94ba4906d64d2d646b5105a13e3a2e324dc9
clarkmyfancy <clark.jack0@gmail.com>
2019-10-20 14:52:44 -0500

    implemented cross-validation folding

--- PredictingForestFires/script.py
+++ PredictingForestFires/script.py
@@ -26,7 +26,51 @@ def generateHistogramUsingOutputFrom(data):
 
 def main():
 
-    test_data = 'hw3_question1.csv'
+    raw_data_set = 'hw3_question1.csv'
+    shuffled_data_set = 'shuffled_q1_data.csv'
+    small_data_set = 'small_data_subset.csv'
+    even_smaller_data_set = 'even_smaller_data_subset.csv'
+    
+    # generateHistogramUsingOutputFrom(raw_data_set)
+    
+
+    data = pd.read_csv(even_smaller_data_set, header = None)
+    num_rows = len(data)
+    num_data_chunks = 3
+    num_data_partitions = math.ceil(num_rows / num_data_chunks)
+
+    
+    chunk_start_index = 0
+    data_chunk_size = math.ceil(num_rows / num_data_partitions)
+    chunk_end_index = data_chunk_size - 1
+    for n in range(0, num_rows, data_chunk_size):
+        Xs = data.drop(data.index[chunk_start_index:chunk_end_index + 1])
+        Ys = data.loc[chunk_start_index:chunk_end_index]
+        chunk_start_index += data_chunk_size
+        chunk_end_index += data_chunk_size
+        print("Xs")
+        print(Xs)
+        print()
+        print("Ys")
+        print(Ys)
+        print("----------------")
+        print()
+    
+    # print("number of rows: " + str(num_rows))
+    print("size of data chunk: " + str(data_chunk_size))
+    # print(num_data_partitions)
+    # print(Xs)
+    # print(Ys)
+    
+
+    print("no errors")
+
+
+
+if __name__ == '__main__':
+    main()
+
+
     # features = [
     #     'x-axis spatial coordinate of forest',    # [1,9]
     #     'y-axis spatial coordinate of forest',    # [2,9]
@@ -42,16 +86,3 @@ def main():
     #     'Rain',                                   # mm/m^2
     #     'Area Burned'
     # ]
-    
-    # generateHistogramUsingOutputFrom(test_data)
-
-    data_set = pd.read_csv(test_data, header = None)
-    suffled_data_set = data_set.sample(frac = 1)
-    suffled_data_set.to_csv('shuffled_q1_data.csv')
-
-    print("no errors")
-
-
-
-if __name__ == '__main__':
-    main()
