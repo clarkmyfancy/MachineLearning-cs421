d0df2046a6913dec343723037851efd600743d43
clarkmyfancy <clark.jack0@gmail.com>
2019-10-20 20:03:08 -0500

    renamed data file to better reflect what it is

--- ModelingNumberOfTicketsSold/script.py
+++ ModelingNumberOfTicketsSold/script.py
@@ -1,5 +1,14 @@
+import csv
+import matplotlib.pyplot as plot 
+import numpy as np 
+import pandas as pd 
+import math 
+from sklearn.linear_model import LogisticRegression
+
 def main():
-    pass
+
+    data_set = 'ticket_sales_data.csv'
+    data = pd.read_csv(data_set, header = None)
 
 if __name__ == '__main__':
     main()
