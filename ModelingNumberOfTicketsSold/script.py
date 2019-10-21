a38b960130e32af2e89ef3d82ab2f9287ecf2af5
clarkmyfancy <clark.jack0@gmail.com>
2019-10-21 12:27:04 -0500

    q2.2 finished

--- ModelingNumberOfTicketsSold/script.py
+++ ModelingNumberOfTicketsSold/script.py
@@ -24,12 +24,36 @@ def generateHistogramFrom(data):
         plot.ylabel('TicketsSold')
         plot.show()
 
+def getFirstMonthsSales(data):
+     with open(data) as d:
+        CsvData = csv.reader(d, delimiter = ',')
+        datetimes = []
+        ticketsSold = []
+        endDatetime = datetime.strptime('25-09-2012 00:00', '%d-%m-%Y %H:%M')
+        for i, row in enumerate(CsvData):
+            # skip first row (contains feature titles)
+            if i == 0:
+                continue
+
+            currentRowDatetime = datetime.strptime(row[0], '%d-%m-%Y %H:%M')
+            if currentRowDatetime >= endDatetime:
+                break
+            datetimes.append(currentRowDatetime)
+            ticketsSold.append(int(row[1]))
+        plot.bar(datetimes, ticketsSold, color = 'blue')
+        plot.xlabel('DateTime')
+        plot.ylabel('TicketsSold')
+        plot.show()
+            
+
 def main():
 
     data_set = 'ticket_sales_data.csv'
     data_subset = 'ticket_sales_data_small_subset.csv'
-    plot.scatter
-    generateHistogramFrom(data_set)
+    # generateHistogramFrom(data_set)
+
+    # first_month_sales = getFirstMonthsSales(data_set)
+    getFirstMonthsSales(data_set)
 
 if __name__ == '__main__':
     main()
