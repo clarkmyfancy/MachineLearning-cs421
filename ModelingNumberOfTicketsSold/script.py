5045128bc186873bb13e83728958338959662382
clarkmyfancy <clark.jack0@gmail.com>
2019-10-21 12:12:45 -0500

    q2.1 finished

--- ModelingNumberOfTicketsSold/script.py
+++ ModelingNumberOfTicketsSold/script.py
@@ -3,6 +3,7 @@ import pandas as pd
 import plotly.express as px
 import matplotlib.pyplot as plot 
 import numpy as np 
+from datetime import datetime
 
 import math 
 from sklearn.linear_model import LogisticRegression
@@ -10,15 +11,15 @@ from sklearn.linear_model import LogisticRegression
 def generateHistogramFrom(data):
     with open(data) as d:
         CsvData = csv.reader(d, delimiter = ',')
-        datetime = []
+        datetimes = []
         ticketsSold = []
         for i, row in enumerate(CsvData):
             # skip first row (contains feature titles)
             if i == 0:
                 continue
-            datetime.append(row[0])
+            datetimes.append(datetime.strptime(row[0], '%d-%m-%Y %H:%M'))
             ticketsSold.append(row[1])
-        plot.plot_date(datetime, ticketsSold, color = 'blue')
+        plot.bar(datetimes, ticketsSold, color = 'blue')
         plot.xlabel('DateTime')
         plot.ylabel('TicketsSold')
         plot.show()
@@ -26,6 +27,7 @@ def generateHistogramFrom(data):
 def main():
 
     data_set = 'ticket_sales_data.csv'
+    data_subset = 'ticket_sales_data_small_subset.csv'
     plot.scatter
     generateHistogramFrom(data_set)
 
